05c80f9b120ff08b6391dd5873384e2b6d24e2b5
rm.mahady <rm.mahady@gmail.com>
2021-06-23 14:26:23 +0300

    Add .env example

--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -5,13 +5,17 @@ import { CarItem } from '../components';
 import { Loading } from '../components/Loading/Loading';
 
 const Home = () => {
-  const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState<any[]>();
   const [loading, setLoading] = useState<boolean>(true);
-
+  const [error, setError] = useState<string>();
   useEffect(() => {
     const abortController = new AbortController();
     listCar(abortController.signal).then((data) => {
-      setCars(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCars(data);
+      }
       setLoading(false);
       console.log('--------home---------', data);
     });
@@ -24,9 +28,17 @@ const Home = () => {
         <Loading />
       ) : (
         <Box mt={4}>
-          {cars.map((car, index) => {
-            return <CarItem key={`car-${index}`} car={car} />;
-          })}
+          {error ? (
+            <Box>
+              <Typography>{error}</Typography>
+            </Box>
+          ) : (
+            <>
+              {(cars || []).map((car, index) => {
+                return <CarItem key={`car-${index}`} car={car} />;
+              })}
+            </>
+          )}
         </Box>
       )}
     </Box>
